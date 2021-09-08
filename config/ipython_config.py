cdc7d7ec52e683e828b269bd9360bbb540da70e7
GitHub Action <action@github.com>
2021-09-08 10:54:48 +0000

    Update documentation

--- config/ipython_config.py
+++
@@ -1,4 +0,0 @@
-from traitlets.config import Config
-c = Config() if 'c' not in locals() else c
-
-c.InteractiveShellApp.extensions = ['djlab']
--- utilities/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-setup(
-    name='djlab',
-    version='0.0.0',
-    py_modules=['djlab'],
-)
