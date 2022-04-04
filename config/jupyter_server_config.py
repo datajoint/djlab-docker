c5525aedd5c867bef552451dd96fe0334d705fa5
A-Baji <bidabaji@gmail.com>
2022-04-04 16:46:24 -0500

    apply requested changes

--- config/jupyter_server_config.py
+++ config/jupyter_server_config.py
@@ -1,29 +1,31 @@
 # Configuration file for jupyter server.
 import hashlib
 import random
-
 from os import getenv, getuid
 from pwd import getpwall
 from traitlets.config import Config
 from djlab import get_djlab_config
 
 user = [u for u in getpwall() if u.pw_uid == getuid()][0]
-c = Config() if 'c' not in locals() else c
+c = Config() if "c" not in locals() else c
+
+
+def passwd(passphrase: str):
+    salt_len = 12
+    h = hashlib.new("sha1")
+    salt = ("%0" + str(salt_len) + "x") % random.getrandbits(4 * salt_len)
+    h.update(passphrase.encode("utf-8") + salt.encode("ascii"))
+    return ":".join(("sha1", salt, h.hexdigest()))
+
 
 # The password to use i.e. "datajoint".
-salt_len = 12
-h = hashlib.new('sha1')
-salt = ('%0' + str(salt_len) + 'x') % random.getrandbits(4 * salt_len)
-h.update(get_djlab_config(
-    'djlab.jupyter_server.password').encode('utf-8') + salt.encode('ascii'))
-
-c.ServerApp.password = ':'.join(('sha1', salt, h.hexdigest()))
+c.ServerApp.password = passwd(get_djlab_config("djlab.jupyter_server.password"))
 
 # Allow root access.
 c.ServerApp.allow_root = False
 
 # IP to serve on.
-c.ServerApp.ip = u'0.0.0.0'
+c.ServerApp.ip = "0.0.0.0"
 
 # Port to serve on.
 c.ServerApp.port = 8888
@@ -32,38 +34,41 @@ c.ServerApp.port = 8888
 c.ServerApp.root_dir = user.pw_dir
 
 # Shell to use for terminal sessions
-c.ServerApp.terminado_settings = {'shell_command': [user.pw_shell]}
+c.ServerApp.terminado_settings = {"shell_command": [user.pw_shell]}
 
 # Root for the file navigation tree menu
-c.FileContentsManager.root_dir = '/home'
+c.FileContentsManager.root_dir = "/home"
 
 # Default browser url to redirect user after login
 # Note: you may also use a query param to modify tree navigation on left
 # e.g. localhost:8888/lab/workspaces/main/tree/dja/hi.md?file-browser-path=/dja
-c.LabApp.default_url = ('/lab' if get_djlab_config(
-                            'djlab.jupyter_server.display_filepath') == 'NULL'
-                        else '/lab/tree{}'.format(
-                            get_djlab_config(
-                                'djlab.jupyter_server.display_filepath').replace(
-                                    c.FileContentsManager.root_dir, '')))
+c.LabApp.default_url = (
+    "/lab"
+    if get_djlab_config("djlab.jupyter_server.display_filepath") == "NULL"
+    else "/lab/tree{}".format(
+        get_djlab_config("djlab.jupyter_server.display_filepath").replace(
+            c.FileContentsManager.root_dir, ""
+        )
+    )
+)
 
 
 def scrub_output_pre_save(model, **kwargs):
     """scrub output before saving notebooks"""
-    if not get_djlab_config('djlab.jupyter_server.save_output').upper() == 'TRUE':
+    if not get_djlab_config("djlab.jupyter_server.save_output").upper() == "TRUE":
         # only run on notebooks
-        if model['type'] != 'notebook':
+        if model["type"] != "notebook":
             return
         # only run on nbformat v4
-        if model['content']['nbformat'] != 4:
+        if model["content"]["nbformat"] != 4:
             return
 
-        model['content']['metadata'].pop('signature', None)
-        for cell in model['content']['cells']:
-            if cell['cell_type'] != 'code':
+        model["content"]["metadata"].pop("signature", None)
+        for cell in model["content"]["cells"]:
+            if cell["cell_type"] != "code":
                 continue
-            cell['outputs'] = []
-            cell['execution_count'] = None
+            cell["outputs"] = []
+            cell["execution_count"] = None
     else:
         return
 
